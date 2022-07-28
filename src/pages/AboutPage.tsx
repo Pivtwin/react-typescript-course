691f9b1093f7f1a7ec7990ea1ff074d9d8b4d81f
Pivtwin <piv4prog@gmail.com>
2022-07-28 10:56:52 +0700

    react-typescript-practise

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet eaque,
+      itaque quos a impedit saepe corporis magni possimus quas corrupti sunt
+      autem distinctio labore quod. Quia natus, omnis ipsam molestias sequi ut
+      assumenda possimus veritatis ab quis delectus, est voluptatum error! Illo
+      blanditiis corrupti animi minima ex temporibus eligendi? Optio.
+    </div>
+  );
+}
